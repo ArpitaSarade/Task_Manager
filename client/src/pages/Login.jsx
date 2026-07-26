6b062f094f58ca4af123f9821a1899d12d7d3bb5
arpita sarade <saradearpita@gmail.com>
2026-07-26 15:28:15 +0530

    add state management to login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+
+  console.log("Email:", email);
+  console.log("Password:", password);
+  };
   return (
     <div className="container vh-100 d-flex justify-content-center align-items-center">
       <div className="card shadow p-4" style={{ width: "400px" }}>
         <h2 className="text-center mb-4">Task Manager</h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           {/* Email */}
           <div className="mb-3">
             <label className="form-label">Email</label>
@@ -15,6 +25,8 @@ function Login() {
               type="email"
               className="form-control"
               placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -26,6 +38,8 @@ function Login() {
               type="password"
               className="form-control"
               placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
