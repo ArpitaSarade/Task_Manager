74c14952fb59b93bd4d760028a17acddb0e34513
arpita sarade <saradearpita@gmail.com>
2026-07-26 15:04:06 +0530

    create reusable layout components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <h3 className="text-white m-0">Task Manager</h3>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,35 @@
 function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <div className="container vh-100 d-flex justify-content-center align-items-center">
+      <div className="card p-4 shadow" style={{ width: "400px" }}>
+        <h2 className="text-center mb-4">Task Manager</h2>
+
+        <form>
+          <div className="mb-3">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter Email"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter Password"
+            />
+          </div>
+
+          <button className="btn btn-primary w-100">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
