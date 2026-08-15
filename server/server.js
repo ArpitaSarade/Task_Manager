d22c5d53877797306aac00d548cf885a32c02f4d
arpita sarade <saradearpita@gmail.com>
2026-08-15 23:19:19 +0530

    Fix CORS for Vercel backend

--- server/server.js
+++ server/server.js
@@ -11,7 +11,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
 app.use(express.json());
 
 // Connect MongoDB
