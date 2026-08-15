57a41b76c676105730ba69bde0668c4ad9ee0760
arpita sarade <saradearpita@gmail.com>
2026-08-15 22:33:17 +0530

    Prepare backend for Vercel

--- server/server.js
+++ server/server.js
@@ -9,19 +9,26 @@ import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Connect MongoDB
+connectDB();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
 app.use("/api/tasks", taskRoutes);
 
-const PORT = process.env.PORT || 5000;
+// Export app for Vercel
+export default app;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Local development
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
